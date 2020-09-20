1d5493910c9e916a65fb71be58e4dd32b1dceaf3
paramonov404 <Oleg_falcon@mail.ru>
2020-09-20 23:24:57 +0300

    PR_4_Java

---
+++ PR_4/src/PR_4_Java/Square.java
@@ -0,0 +1,38 @@
+package PR_4_Java;
+
+public class Square extends Rectangle {
+
+    public Square() {
+
+    }
+
+    public Square(double side) {
+        super(side,side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side,side,color,filled);
+    }
+
+    public double getSide() {
+        return width;
+    }
+
+    public void setSide(double side) {
+        width = side;
+        length = side;
+    }
+
+    public void setWidth(double side) {
+        this.width = side;
+    }
+
+    public void setLength(double side) {
+        this.length = side;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
