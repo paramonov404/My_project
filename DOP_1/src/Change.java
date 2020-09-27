93a18f215393a45c3dce5237814ff790f4b4cbe1
paramonov404 <Oleg_falcon@mail.ru>
2020-09-27 22:07:58 +0300

    Dop_1

---
+++ DOP_1/src/Change.java
@@ -0,0 +1,20 @@
+public class Change {
+    public int search(int x, int y) {
+        if (x == 0)
+            return 1;
+        if (x < 0)
+            return 0;
+        if (y == 5)
+            return search(x - 5, 5) + search(x - 3, 3) + search(x - 1, 1);
+        if (y == 3)
+            return search(x - 3, 3) + search(x - 1, 1);
+        if (y == 1)
+            return search(x - 1, 1);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Change change = new Change();
+        System.out.println(change.search(4, 5));
+    }
+}
